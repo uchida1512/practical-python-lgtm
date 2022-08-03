26519b3544a9c3e6487629076f7dcbb2432d4a62
uchida1512 <uchida1512@gmail.com>
2022-08-03 22:50:56 +0900

    コマンドライン引数（画像のソース情報とメッセージ）を受け取れるようにする

--- lgtm/core.py
+++ lgtm/core.py
@@ -3,11 +3,15 @@ import click
 
 
 # Click パッケージで作成するコマンドラインツールは、
-# デコレータclick.command() を付けた関数を呼び出すと実行できる
+# デコレータ click.command() を付けた関数を呼び出すと実行できる。
+# また、デコレータ click.option() で、オプションで渡すものを指定し
+# デコレータ click.argument() で、位置引数で渡すものを指定する。
 @click.command()
-def cli():
+@click.option('--message', '-m', default='LGTM', show_default=True, help='画像に乗せる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
     """LGTM 画像作成ツール"""
-    lgtm()
+    lgtm(keyword, message)
     # 動作確認用
     click.echo('lgtm')
 
@@ -15,3 +19,14 @@ def cli():
 def lgtm():
     # ここにロジック(処理)を追加していく
     pass
+
+
+# a@DESKTOP-M8KF2FB MINGW64 ~/PycharmProjects/pythonProject/practical-python-lgtm (main)
+# $ python main.py --help
+# Usage: main.py [OPTIONS] KEYWORD
+#
+#   LGTM 画像作成ツール
+#
+# Options:
+#   -m, --message TEXT  画像に乗せる文字列  [default: LGTM]
+#   --help              Show this message and exit.
